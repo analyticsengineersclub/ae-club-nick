27116e2559d41d4b53df799e9ec21632aef7a7fb
Nick Bosma <nick.bosma@educationperfect.com>
2021-08-26 07:17:06 +0000

    Change inner join to left join to ensure we can find customers who have never ordered

--- customers.sql
+++ customers.sql
@@ -1,10 +1,10 @@
 select
-    orders.customer_id,
+    customers.id as customer_id,
     customers.name,
     customers.email,
     min(orders.created_at) as first_order_at,
-    count(distinct orders.id) as number_of_orders
+    coalesce(count(distinct orders.id),0) as number_of_orders
 from `analytics-engineers-club.coffee_shop.orders` as orders
-inner join `analytics-engineers-club.coffee_shop.customers` as customers on orders.customer_id = customers.id
+left outer join `analytics-engineers-club.coffee_shop.customers` as customers on orders.customer_id = customers.id
 group by 1,2,3
 order by first_order_at
